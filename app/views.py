53332a376503e4fafcfbc318d4df1abc9761a2cf
cjkenned <cam.j.kennedy@gmail.com>
2014-11-11 22:17:13 -0700

    Styling changes, remove unneeded files

--- app/views.py
+++ app/views.py
@@ -6,3 +6,13 @@ from flask import make_response
 @app.route('/index')
 def index():
     return make_response(open('app/templates/index.html').read())
+
+
+@app.route('/about')
+def about():
+    pass
+
+
+@app.route('/contact')
+def contact():
+    pass
