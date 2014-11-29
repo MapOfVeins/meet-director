0de0b0c01b3315afbf36277a497dca637f8d1fc7
cjkenned <cam.j.kennedy@gmail.com>
2014-11-29 15:56:45 -0700

    Update create meet styles and flows

--- app/static/js/controllers/CreateMeetController.js
+++ app/static/js/controllers/CreateMeetController.js
@@ -9,13 +9,60 @@
             title: '',
             fed: '',
             director: '',
-            startDate: ''
+            startDate: '',
+            lifters: []
         };
+
+        $scope.currentLifter = {};
+        $scope.weightClasses = [];
+        $scope.femaleWeightClasses = [
+            '43kg',
+            '47kg',
+            '52kg',
+            '57kg',
+            '63kg',
+            '72kg',
+            '84kg',
+            '84kg+'
+        ];
+        $scope.maleWeightClasses = [
+            '53kg',
+            '59kg',
+            '66kg',
+            '74kg',
+            '83kg',
+            '93kg',
+            '105kg',
+            '120kg',
+            '120kg+'
+        ];
 
         $scope.currentStage = PARTIALS_PATH + 'initMeet.html';
 
         $scope.setCurrentStage = function(stage) {
             $scope.currentStage = stage;
+        };
+
+        $scope.addNewLifter = function() {
+            $scope.meet.lifters.push($scope.currentLifter);
+            $scope.currentLifter = {};
+        };
+
+        $scope.switchWeightClasses = function () {
+            console.log($scope.currentLifter);
+            if ($scope.currentLifter.gender === 'male') {
+                $scope.weightClasses = $scope.maleWeightClasses;
+            } else {
+                $scope.weightClasses = $scope.femaleWeightClasses;
+            }
+        };
+
+        $scope.returnToInit = function() {
+            $scope.currentStage = PARTIALS_PATH + 'initMeet.html';
+        };
+
+        $scope.removeLifter = function(lifter) {
+            $scope.meet.lifters.splice($scope.meet.lifters.indexOf(lifter), 1);
         };
 
     }]);
